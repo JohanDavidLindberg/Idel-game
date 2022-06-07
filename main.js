6554fa127374550db4d7dc72479855a39866fcd0
JohanDavidLindberg <94990546+JohanDavidLindberg@users.noreply.github.com>
2022-06-07 13:29:08 +0200

    reviderad första

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
+var pengar = 0;
+var pps = 0;
+var klickpeng = 1;
+
+let första = 0;
+let förstakost = 10;
 
 
 
 
-let kaka = document.getElementById("klikare");
+function köpaförsta() {
+    if(pengar >= förstakost) {
+        första++
+        pengar -= förstakost
+        förstakost += förstakost *1.05
+        
+    }
+    förstakost = Math.round(förstakost * 10)/10;
+    document.getElementById("förstaantal").textContent = första;
+    document.getElementById("förstakost").textContent = "Kostand: " + förstakost;
+}
 
 
+setInterval(function(){ 
+    pps = 0;
+    klickpeng = 1;
+    klickpeng += första * 0.1;
+
+
+    pps += första;
+    
+    
+    
+    
+    
+    pengar += pps/10
+    klickpeng = Math.round(klickpeng * 10)/10;
+    pengar = Math.round(pengar * 10)/10;
+    document.getElementById("antalpengar").textContent = pengar;
+    document.getElementById("pps").textContent = pps + " pps";
+
+}, 100);
 
 function kakklick() {
+    pengar += klickpeng;
 
+    pengar = Math.round(pengar * 10)/10;
 
+    document.getElementById("antalpengar").textContent = pengar;
+    console.log(pengar)
 }
 
 
